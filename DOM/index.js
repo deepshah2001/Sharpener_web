629585067682cefbe9d691bdf45a9e0d5725b6e5
Deep Shah <shahdeep020@gmail.com>
2023-07-26 14:42:04 +0530

    DOM Introduction - 4

--- DOM/index.js
+++ DOM/index.js
@@ -32,12 +32,46 @@
 // }
 
 // 3. getElementByTagName
-var li = document.getElementsByTagName("li");
-console.log(li);
-console.log(li[0]);
+// var li = document.getElementsByTagName("li");
+// console.log(li);
+// console.log(li[0]);
 
-li[2].style.backgroundColor = "green";
+// li[2].style.backgroundColor = "green";
 
-for(let i = 0; i < li.length; ++i) {
-    li[i].style.fontWeight = "bold";
+// for(let i = 0; i < li.length; ++i) {
+//     li[i].style.fontWeight = "bold";
+// }
+
+// 4. querySelector
+// var header = document.querySelector("#main-header");
+// header.style.border = "solid 3px black";
+
+// var input = document.querySelector("input");
+// input.value = "Hello, World";
+
+// var submit = document.querySelector('input[type="submit"]');
+// submit.value = "SEND";
+
+// var item = document.querySelector(".list-group-item");
+// item.style.color = "green";
+
+// var lastItem = document.querySelector(".list-group-item:last-child");
+// lastItem.style.color = "blue";
+
+var secondItem = document.querySelector(".list-group-item:nth-child(2)");
+secondItem.style.backgroundColor = "green";
+
+var thirdItem = document.querySelector(".list-group-item:nth-child(3");
+thirdItem.style.display = "none";
+
+// 5. querySelectorAll
+var items = document.querySelectorAll(".list-group-item");
+// console.log(items);
+items[1].style.color = "green";
+
+var odd = document.querySelectorAll("li:nth-child(odd)");
+// console.log(odd);
+
+for(let i = 0; i < odd.length; i++) {
+    odd[i].style.backgroundColor = "green";
 }
