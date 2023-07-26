cab1e7202f28fc1b1196842266e239d197fe9911
Deep Shah <shahdeep020@gmail.com>
2023-07-26 14:03:00 +0530

    DOM Introduction - 2

--- DOM/index.js
+++ DOM/index.js
@@ -11,11 +11,23 @@
 // var headerTilte = document.getElementById("header-title");
 // console.log(header);
 
-var header = document.getElementById("main-header");
+// var header = document.getElementById("main-header");
 
-header.style.border = "solid 3px black";
+// header.style.border = "solid 3px black";
 
-var addItem = document.querySelector(".title");
+// var addItem = document.querySelector(".title");
 // console.log(addItem);
-addItem.style.fontWeight = "bold";
-addItem.style.color = "green";
+// addItem.style.fontWeight = "bold";
+// addItem.style.color = "green";
+
+// 2. getElementsByClassName
+var items = document.getElementsByClassName("list-group-item");
+console.log(items);
+console.log(items[0]);
+
+items[2].style.backgroundColor = "green";
+
+for(let i = 0; i < items.length; i++) {
+    items[i].style.fontWeight = "bold"; 
+}
+
