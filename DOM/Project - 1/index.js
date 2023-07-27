d225b3a180be88030ea224b36dba3eba4978e380
Deep Shah <shahdeep020@gmail.com>
2023-07-27 12:26:14 +0530

    Filter added

--- DOM/Project - 1/index.js
+++ DOM/Project - 1/index.js
@@ -1,5 +1,6 @@
 let form = document.getElementById('addForm');
 let itemList = document.getElementById('items');
+let filterItem = document.getElementById('filter');
 
 // Form Submit event
 form.addEventListener('submit', addItem);
@@ -7,12 +8,18 @@ form.addEventListener('submit', addItem);
 // Delete Item
 itemList.addEventListener('click', removeItem);
 
+// Search Item
+filterItem.addEventListener('keyup', filter);
+
 // Add Item
 function addItem(e) {
     e.preventDefault();
     
     // value of textfield store in li
-    let li = document.getElementById('item').value;
+    let l1 = document.getElementById('item1').value;
+    let l2 = document.getElementById('item2').value;
+
+    let li = l1 + " " + l2;
 
     if(li === '')
         alert("Write something!");
@@ -25,6 +32,18 @@ function addItem(e) {
 
         // append child
         item.appendChild(document.createTextNode(li));
+
+        // Edit button in every task
+        let editBtn = document.createElement('button');
+
+        // Adding class to edit button
+        editBtn.className = "btn btn-secondary btn-sm float-right edit";
+
+        // Add text 'Edit' to the edit button
+        editBtn.appendChild(document.createTextNode('Edit'));
+
+        // Append edit button to the item created newly
+        item.appendChild(editBtn);
 
         // delete button in every task
         var deleteBtn = document.createElement('button');
@@ -51,5 +70,34 @@ function removeItem(e) {
             var li = e.target.parentElement;
             itemList.removeChild(li);
         }
+    }
+}
+
+// Search or filter item
+function filter(e) {
+    // Search value stored in text variable
+    let text = e.target.value.toLowerCase();
+    // Get lists
+    let items = itemList.getElementsByTagName('li');
+
+    // Checking if the search value is not empty
+    if(text != '') {
+        // Convert the HTML Collection into Array
+        Array.from(items).forEach(function(item) {
+            
+            // Items in the lists of item i.e the actual text
+            let itemName = item.firstChild.textContent;
+
+            // Checking if text is present in itemName or not
+            if(itemName.toLowerCase().indexOf(text) != -1) {
+                item.style.display = 'block';
+            } else {
+                item.style.display = 'none';
+            }
+        });
+    } else {
+        Array.from(items).forEach(function(item) {
+            item.style.display = 'block';
+        })
     }
 }
