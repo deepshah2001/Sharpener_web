c012b2eaf077f42cdefc96054a275364ebc66adc
Deep Shah <shahdeep020@gmail.com>
2023-08-05 15:05:56 +0530

    Booking Appointment using crudcrud (POST)

---
+++ Booking App - 2/index.js
@@ -0,0 +1,99 @@
+let submitBtn = document.getElementById('submit');
+let ul = document.getElementById('lists');
+let form = document.getElementById('form');
+
+submitBtn.addEventListener('click', submit);
+
+let userName = document.getElementById('name');
+let email = document.getElementById('email');
+let phone = document.getElementById('phone');
+
+// Retreiving already present data in the databse (cloud) using array of objects (JSON)
+axios.get('https://crudcrud.com/api/768204b305354e72956bc9e0d7f5277a/appointmentData')
+    .then((response) => {
+        response.data.forEach(element => {
+            showUsers(element);
+        });
+    })
+    .catch((err) => console.error(err));
+
+function submit(e) {
+    e.preventDefault();
+
+    if(userName.value === '' || email.value === '' || phone.value === '') {
+        alert("Fill out the values in the input field.");
+    } else {
+        // User Data to store
+        let myUser = {
+            'name': userName.value,
+            'email': email.value,
+            'phone': phone.value
+        }
+        
+        //  Adding new appointment of the user into the database (backend)
+        axios.post('https://crudcrud.com/api/768204b305354e72956bc9e0d7f5277a/appointmentData', myUser)
+            .then((response) => showUsers(response.data))
+            .catch((err => console.error(err)));
+        ;
+    }
+}
+
+function deleteTask(myUser, li) {
+    // For taking out the email or key of local storage
+    let key = myUser.email;
+
+    // removing it from local storage
+    localStorage.removeItem(key);
+
+    // removing the list displayed below the form
+    ul.removeChild(li);
+}
+
+function showUsers(myUser) {
+    // Creating a new 'li' element
+    var li = document.createElement('li');
+
+    // Text node for 'li' element to be displayed below the form
+    let text = JSON.stringify(myUser.name) + " - " + JSON.stringify(myUser.email) + " - " + JSON.stringify(myUser.phone);
+    li.appendChild(document.createTextNode(text));
+
+    // Creating delete button
+    let deleteBtn = document.createElement('button');
+
+    // Add Text to the delete button
+    deleteBtn.append(document.createTextNode("Delete"));
+
+    li.appendChild(deleteBtn);
+
+    // Created Edit Button
+    let editBtn = document.createElement('button');
+
+    // Adding text to edit button
+    editBtn.append(document.createTextNode("Edit"));
+
+    // Adding edit button to list
+    li.appendChild(editBtn);
+    
+    // Displaying the new user in list
+    ul.appendChild(li);
+
+    form.reset();
+
+    // Delete Button if clicked
+    deleteBtn.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        deleteTask(myUser, li);
+    })
+
+    // Edit button if clicked
+    editBtn.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        userName.value = myUser.name;
+        email.value = myUser.email;
+        phone.value = myUser.phone;
+
+        deleteTask(myUser, li);
+    })
+}
