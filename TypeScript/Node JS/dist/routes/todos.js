ad7065f07bbdfca5a03db51bacbd2ada0ab6f600
Koustubh Shah <90774217+deepshah2001@users.noreply.github.com>
2023-08-25 17:25:47 +0530

    Typescript showdown

--- TypeScript/Node JS/dist/routes/todos.js
+++ TypeScript/Node JS/dist/routes/todos.js
@@ -7,23 +7,27 @@ router.get("/", (req, res, next) => {
     res.status(200).json({ todos: todos });
 });
 router.post("/post", (req, res, next) => {
+    const body = req.body;
     const newToDo = {
         id: new Date().toISOString(),
-        text: req.body.text,
+        text: body.text,
     };
     todos.push(newToDo);
     res.status(200).json({ todos: todos });
 });
 router.post("/delete/:id", (req, res, next) => {
-    const id = req.params.id;
+    const params = req.params;
+    const id = params.id;
     todos = todos.filter(item => item.id !== id);
     res.status(200).json({ todos: todos });
 });
 router.post("/edit/:id", (req, res, next) => {
-    const id = req.params.id;
+    const params = req.params;
+    const body = req.body;
+    const id = params.id;
     const index = todos.findIndex(item => item.id === id);
     if (index) {
-        todos[index] = { id: todos[index].id, text: req.body.text };
+        todos[index] = { id: todos[index].id, text: body.text };
         res.status(200).json({ todos: todos });
     }
     else {
--- TypeScript/Node JS/src/routes/todos.ts
+++ TypeScript/Node JS/src/routes/todos.ts
@@ -2,6 +2,9 @@ import { Router } from 'express';
 
 import { ToDo } from '../models/todos';
 
+type RequestBody = { text: string };
+type RequestParams = { id: string };
+
 let todos: ToDo[] = [];
 
 const router = Router();
@@ -11,9 +14,11 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/post", (req, res, next) => {
+    const body = req.body as RequestBody;
+
     const newToDo: ToDo = {
         id: new Date().toISOString(),
-        text: req.body.text,
+        text: body.text,
     };
 
     todos.push(newToDo);
@@ -21,18 +26,23 @@ router.post("/post", (req, res, next) => {
 });
 
 router.post("/delete/:id", (req, res, next) => {
-    const id = req.params.id;
+    const params = req.params as RequestParams;
+
+    const id = params.id;
     todos= todos.filter(item => item.id !== id);
 
     res.status(200).json({todos: todos});
 });
 
 router.post("/edit/:id", (req, res, next) => {
-    const id = req.params.id;
+    const params = req.params as RequestParams;
+    const body = req.body as RequestBody;
+    
+    const id = params.id;
     const index = todos.findIndex(item => item.id === id);
 
     if(index) {
-        todos[index] = {id: todos[index].id, text: req.body.text};
+        todos[index] = {id: todos[index].id, text: body.text};
         res.status(200).json({todos: todos});
     } else {
         res.status(404);
